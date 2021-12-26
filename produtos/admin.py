63547cf435b0c772f4f11bbfd9e8330126f94aa8
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2021-12-26 13:53:37 -0400

    add product to admin

--- produtos/admin.py
+++ produtos/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+
+class ProdutoAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+
+
+admin.site.register(Produto, ProdutoAdmin)
