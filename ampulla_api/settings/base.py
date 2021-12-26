a0b13163d6e5858003d3f7cd825d85a6b935976b
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2021-12-26 13:51:30 -0400

    fix allowed hosts in dev ambient to any host

--- ampulla_api/settings/base.py
+++ ampulla_api/settings/base.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-822)-+w=&xii%*_j=+)xloa_)=k%vscx$^6#_)ycn+5q0e#in9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
