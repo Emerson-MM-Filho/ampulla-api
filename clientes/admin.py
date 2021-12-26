2cb51d6f76100008e4cdde6ed65c9b9b96ff989b
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2021-12-26 13:51:56 -0400

    show more fields in client admin

--- clientes/admin.py
+++ clientes/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 
 from clientes.models import Cliente
 
-admin.site.register(Cliente)
+
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['name', 'phone', 'street', 'number', 'district']
+
+
+admin.site.register(Cliente, ClienteAdmin)
+admin.site.disable_action('delete_selected')
