0b41ced0df831e7051036f96eb5172b8a78c21b6
Emerson-MM-Filho <emerson.dev.machado@gmail.com>
2021-12-26 13:53:16 -0400

    create product model

--- produtos/models.py
+++ produtos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Produto(models.Model):
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    name = models.CharField(
+        max_length=75, null=False, blank=False, verbose_name='nome'
+    )
+
+    price = models.DecimalField(
+        max_digits=4, decimal_places=2, null=False, blank=False, verbose_name='preço'
+    )
+
+    def __str__(self):
+        return self.name
